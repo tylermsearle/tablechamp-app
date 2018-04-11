74fbc40f83c6ad30d2283bc5ed402742f2bcd1a4
tylermsearle <31483436+tylermsearle@users.noreply.github.com>
2018-04-11 15:27:23 -0600

    Update firebase-key.js

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
-  "apiKey": '', 
-  "authDomain": '', 
-  "databaseURL": '' 
+  "apiKey": 'AIzaSyCzqfIyWs18CxfAcO-p71MZ6sf77AmMaP0', 
+  "authDomain": 'table-champ-fc7a1.firebaseapp.com', 
+  "databaseURL": 'https://table-champ-fc7a1.firebaseio.com' 
 };
